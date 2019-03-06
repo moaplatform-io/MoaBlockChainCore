f5c5298da1ebfa60b209703895bca31c99e51541
timo <smkim@moaplanet.co.kr>
2019-03-07 04:20:31 +0900

    init

--- src/main/java/io/moaplatform/blockchain/MConfig.java
+++ src/main/java/io/moaplatform/blockchain/MConfig.java
@@ -1,8 +1,10 @@
 package io.moaplatform.blockchain;
 
 public class MConfig {
+	
 	public static class NET_PARAM{
 		public static final String MOA_MAIN_ID ="MOA";
 		public static final String MOA_TEST_ID ="MOA_TEST";
 	}
+	
 }
